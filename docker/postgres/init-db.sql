6bf93c27a4344a4cacd0babe6c8091f215df31c5
Vihanga Kulathilake <vihangajanith12m@gmail.com>
2026-07-13 13:21:24 +0530

    Add Docker support for microservices deployment

---
+++ docker/postgres/init-db.sql
@@ -0,0 +1,5 @@
+-- Create Databases for DevGuardian Services
+CREATE DATABASE devguardian_auth;
+CREATE DATABASE devguardian_repository;
+CREATE DATABASE devguardian_analysis;
+CREATE DATABASE devguardian_notification;
